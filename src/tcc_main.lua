9c2cc4bfafd4d5b57b0963a1d8d5e1bd46f31bf6
samuel <samuel.rocha.castan@gmail.com>
2026-03-26 15:56:25 +0000

    entendendo pico-lua na pratica

--- src/tcc_main.lua
+++ src/tcc_main.lua
@@ -5,10 +5,17 @@ local qnt_cores = 15
 
 pico.set.window{title="Personalização", fullscreen = true}
 local win = pico.get.window()
-local window = win.dim
- local background = {0.5, 0.5, 1,1}
-    Pico_Rel_Rect pinguim = { '%', {0.3, 0.35, 0.3, 0}, PICO_ANCHOR_C, NULL };
-    Pico_Rel_Rect voltar = { '%', {0.3, 0.8, 0.3, 0}, PICO_ANCHOR_C, NULL };
+print(win.dim.w, win.dim.h)
+
+
+local all = pico.get.view()
+print(all.dim.w, all.dim.h)
+
+local background = {'%', x=0.5, y=0.5, w=1, h=1}
+local pinguim = {'%', x=0.3, y=0.35, w=0.3, h=0}
+local voltar = {'%', x=0.3, y=0.8, w=0.3, h=0}
+
+pico.output.draw.image("imgs/background_personalizar.png", background);
 
 pico.input.delay(5000)
 
