4cb48da0c03f9974d9251df6b484a60d189769a7
samuel <samuel.rocha.castan@gmail.com>
2026-03-30 19:49:31 +0000

    add: funções auxiliares para os testes de colisão

--- src/codes_tcc/testes/mouse_layers.lua
+++ src/codes_tcc/testes/mouse_layers.lua
@@ -1,3 +1,27 @@
+function mouse_in_layer(layer_pos)
+    pico.set.layer(layer)
+    local layer_view = pico.get.view()
+    pico.set.layer(nil)
+
+    local mouse = pico.get.mouse('%')
+
+    local w = layer_pos.w
+    local h = layer_pos.h
+
+    local x0 = layer_pos.x - w/2
+    local y0 = layer_pos.y - h/2
+
+    local local_x = (mouse.x - x0) / w
+    local local_y = (mouse.y - y0) / h
+
+    return {
+        '%',
+        x = local_x,
+        y = local_y,
+    }
+end
+
+
 pico.init(true)
 
 pico.layer.empty("ui", {w=100, h=100})
@@ -7,16 +31,16 @@ pico.output.draw.rect(rect_layer)
 local i = 0
 
 pico.set.layer(nil)
-pico.set.expert(true,1)
+pico.set.expert(true,20)
 while true do
-     local e = pico.input.event()
-        mouse = pico.get.mouse()
-        if e ~= nil then
-            if e.tag=='quit' then
-                pico.quit()
-                break
-            end
+    local e = pico.input.event()
+    mouse = mouse_in_layer({'%', x=0.4, y=0.3, w=0.3, h=0.3})
+    if e ~= nil then
+        if e.tag=='quit' then
+            pico.quit()
+            break
         end
+    end
     if pico.vs.pos_rect(mouse, rect_layer) then
         print("DETECTOU COLISAO")
     else
--- src/codes_tcc/testes/rects_layers.lua
+++ src/codes_tcc/testes/rects_layers.lua
@@ -1,20 +1,45 @@
+function rect_in_main(layer_pos, rect)
+    pico.set.layer(nil)
+
+    local w = layer_pos.w
+    local h = layer_pos.h
+
+    local x0 = layer_pos.x - w/2
+    local y0 = layer_pos.y - h/2
+
+    local local_x = x0 + rect.x * w
+    local local_y = y0 + rect.y * h
+    local local_w = rect.w*w
+    local local_h = rect.h*h
+
+    return {
+        '%',
+        x = local_x,
+        y = local_y,
+        w = local_w,
+        h = local_h
+    }
+end
+
+local rect_layer = {'%', x=0.5, y=0.5, w=0.3, h=0.3}
+local rect_main = {'%', x=0.35, y=0.25, w=0.1, h=0.1}
+
 pico.init(true)
 pico.layer.empty("ui", {w=100, h=100})
 pico.set.layer("ui")
-local rect_layer = {'%', x=0.5, y=0.5, w=0.3, h=0.3}
 pico.output.draw.rect(rect_layer)
 
-pico.set.layer(nil)
-pico.set.color.draw('red')
-local rect_main = {'%', x=0.35, y=0.25, w=0.1, h=0.1}
-pico.output.draw.rect(rect_main)
-pico.output.draw.layer("ui", {'%', x=0.4, y=0.3, w=0.3, h=0})
-
-if pico.vs.rect_rect(rect_main, rect_layer) then
+local rect_layer_in_main = rect_in_main({'%', x=0.4, y=0.3, w=0.3, h=0.3}, rect_layer)
+if pico.vs.rect_rect(rect_main, rect_layer_in_main) then
     print("DETECTOU COLISAO")
 else
     print("NAO DETECTOU COLISAO")
 end
+
+pico.set.layer(nil)
+pico.set.color.draw('red')
+pico.output.draw.rect(rect_main)
+pico.output.draw.layer("ui", {'%', x=0.4, y=0.3, w=0.3, h=0.3})
 
 pico.input.delay(3000)
 pico.init(false)
